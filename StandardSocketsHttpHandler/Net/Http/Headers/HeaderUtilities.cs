9ea18cb2114b8aa3997df552ce86c4c1fa33c446
TalAloni <tal.aloni.il@gmail.com>
2020-04-11 02:50:03 +0300

    HeaderUtilities: Removed unused methods

--- StandardSocketsHttpHandler/Net/Http/Headers/HeaderUtilities.cs
+++ StandardSocketsHttpHandler/Net/Http/Headers/HeaderUtilities.cs
@@ -14,57 +14,7 @@ namespace System.Net.Http.Headers
 {
     internal static class HeaderUtilities
     {
-        private const string qualityName = "q";
-
-        internal const string ConnectionClose = "close";
-        internal static readonly TransferCodingHeaderValue TransferEncodingChunked =
-            new TransferCodingHeaderValue("chunked");
-        internal static readonly NameValueWithParametersHeaderValue ExpectContinue =
-            new NameValueWithParametersHeaderValue("100-continue");
-
-        internal const string BytesUnit = "bytes";
-
-        // Validator
-        internal static readonly Action<HttpHeaderValueCollection<string>, string> TokenValidator = ValidateToken;
-
         private static readonly char[] s_hexUpperChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-        internal static void SetQuality(ObjectCollection<NameValueHeaderValue> parameters, double? value)
-        {
-            Debug.Assert(parameters != null);
-
-            NameValueHeaderValue qualityParameter = NameValueHeaderValue.Find(parameters, qualityName);
-            if (value.HasValue)
-            {
-                // Note that even if we check the value here, we can't prevent a user from adding an invalid quality
-                // value using Parameters.Add(). Even if we would prevent the user from adding an invalid value
-                // using Parameters.Add() he could always add invalid values using HttpHeaders.AddWithoutValidation().
-                // So this check is really for convenience to show users that they're trying to add an invalid 
-                // value.
-                if ((value < 0) || (value > 1))
-                {
-                    throw new ArgumentOutOfRangeException(nameof(value));
-                }
-
-                string qualityString = ((double)value).ToString("0.0##", NumberFormatInfo.InvariantInfo);
-                if (qualityParameter != null)
-                {
-                    qualityParameter.Value = qualityString;
-                }
-                else
-                {
-                    parameters.Add(new NameValueHeaderValue(qualityName, qualityString));
-                }
-            }
-            else
-            {
-                // Remove quality parameter
-                if (qualityParameter != null)
-                {
-                    parameters.Remove(qualityParameter);
-                }
-            }
-        }
 
         internal static bool ContainsNonAscii(string input)
         {
@@ -127,45 +77,6 @@ namespace System.Net.Http.Headers
             destination.Append('%');
             destination.Append(s_hexUpperChars[(c & 0xf0) >> 4]);
             destination.Append(s_hexUpperChars[c & 0xf]);
-        }
-
-        internal static double? GetQuality(ObjectCollection<NameValueHeaderValue> parameters)
-        {
-            Debug.Assert(parameters != null);
-
-            NameValueHeaderValue qualityParameter = NameValueHeaderValue.Find(parameters, qualityName);
-            if (qualityParameter != null)
-            {
-                // Note that the RFC requires decimal '.' regardless of the culture. I.e. using ',' as decimal
-                // separator is considered invalid (even if the current culture would allow it).
-                double qualityValue = 0;
-                if (double.TryParse(qualityParameter.Value, NumberStyles.AllowDecimalPoint,
-                    NumberFormatInfo.InvariantInfo, out qualityValue))
-                {
-                    return qualityValue;
-                }
-                // If the stored value is an invalid quality value, just return null and log a warning. 
-                if (NetEventSource.IsEnabled) NetEventSource.Error(null, String.Format(SR.net_http_log_headers_invalid_quality, qualityParameter.Value));
-            }
-            return null;
-        }
-
-        internal static void CheckValidToken(string value, string parameterName)
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                throw new ArgumentException(SR.net_http_argument_empty_string, parameterName);
-            }
-
-            if (HttpRuleParser.GetTokenLength(value, 0) != value.Length)
-            {
-                throw new FormatException(String.Format(System.Globalization.CultureInfo.InvariantCulture, SR.net_http_headers_invalid_value, value));
-            }
-        }
-
-        private static void ValidateToken(HttpHeaderValueCollection<string> collection, string value)
-        {
-            CheckValidToken(value, "item");
         }
     }
 }
