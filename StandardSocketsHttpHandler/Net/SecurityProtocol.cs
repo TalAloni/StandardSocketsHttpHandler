696310b9bc3d160e15e30720c6abedfafd3672c3
TalAloni <tal.aloni.il@gmail.com>
2020-04-11 02:27:37 +0300

    Removed TLS 1.3 From DefaultSecurityProtocols

--- StandardSocketsHttpHandler/Net/SecurityProtocol.cs
+++ StandardSocketsHttpHandler/Net/SecurityProtocol.cs
@@ -9,9 +9,6 @@ namespace System.Net
     internal static class SecurityProtocol
     {
         public const SslProtocols DefaultSecurityProtocols =
-#if !netstandard && !netfx
-            SslProtocols.Tls13 |
-#endif
             SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12;
 
         public const SslProtocols SystemDefaultSecurityProtocols = SslProtocols.None;
