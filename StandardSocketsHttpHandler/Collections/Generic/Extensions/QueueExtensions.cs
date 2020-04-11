6a9b01a022b3e7b349530f066f85d87066cbd627
TalAloni <tal.aloni.il@gmail.com>
2020-04-11 15:22:07 +0300

    Added QueueExtensions

---
+++ StandardSocketsHttpHandler/Collections/Generic/Extensions/QueueExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace System.Collections.Generic
+{
+    internal static class QueueExtensions
+    {
+        public static bool TryDequeue<T>(this Queue<T> queue, out T value)
+        {
+            try
+            {
+                value = queue.Dequeue();
+                return true;
+            }
+            catch(InvalidOperationException)
+            {
+                value = default(T);
+                return false;
+            }
+        }
+    }
+}
