150eaffceb84175c52c95cbdf14e69dba6274b36
TalAloni <tal.aloni.il@gmail.com>
2020-04-11 15:30:21 +0300

    Added MemoryExtensions

---
+++ StandardSocketsHttpHandler/Extensions/MemoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    internal static class MemoryExtensions
+    {
+        public static ArraySegment<byte> GetUnderlyingArray(this Memory<byte> bytes) => GetUnderlyingArray((ReadOnlyMemory<byte>)bytes);
+
+        public static ArraySegment<byte> GetUnderlyingArray(this ReadOnlyMemory<byte> bytes)
+        {
+            if (!MemoryMarshal.TryGetArray(bytes, out var arraySegment))
+            {
+                throw new NotSupportedException("This Memory does not support exposing the underlying array.");
+            }
+            return arraySegment;
+        }
+    }
+}
