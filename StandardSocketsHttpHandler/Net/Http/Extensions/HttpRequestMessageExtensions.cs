39484d303f1a2a72ee120d5ad6019320fdc99d39
Simon Ferquel <simon.ferquel@unity3d.com>
2022-02-02 11:22:37 +0100

    More details in comments
    Signed-off-by: Simon Ferquel <simon.ferquel@unity3d.com>

--- StandardSocketsHttpHandler/Net/Http/Extensions/HttpRequestMessageExtensions.cs
+++ StandardSocketsHttpHandler/Net/Http/Extensions/HttpRequestMessageExtensions.cs
@@ -6,11 +6,12 @@ namespace System.Net.Http
 {
     internal static class HttpRequestMessageExtensions
     {
-        // depending on runtime the backing field for headers is either _headers or headers
-        // _headers in .net core and some .net fx and mono versions.
+        // Depending on runtime the backing field for headers is either _headers or headers:
+        // _headers in .net core and some .net fx and mono versions (.NET Framework 4.6.1, Mono on Mac)
         // headers in other .net fx / mono versions.
         static FieldInfo _headersField = typeof(HttpRequestMessage).GetField("_headers", BindingFlags.Instance | BindingFlags.NonPublic) ??
             typeof(HttpRequestMessage).GetField("headers", BindingFlags.Instance | BindingFlags.NonPublic);
+
         public static bool HasHeaders(this HttpRequestMessage request)
         {
             HttpRequestHeaders headers = (HttpRequestHeaders)_headersField.GetValue(request);
