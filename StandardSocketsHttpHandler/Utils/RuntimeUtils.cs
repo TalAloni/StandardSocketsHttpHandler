0ce3635d6a369b78f255dd74afeab281a290ec17
Misaka-L <lipww1234@foxmail.com>
2025-12-06 17:02:41 +0800

    fix: HttpRequestMessageExtensions.HasHeaders use wrong field name in .net mono

--- StandardSocketsHttpHandler/Utils/RuntimeUtils.cs
+++ StandardSocketsHttpHandler/Utils/RuntimeUtils.cs
@@ -18,5 +18,12 @@ namespace System
             string frameworkDescription = RuntimeInformation.FrameworkDescription;
             return frameworkDescription.StartsWith(DotnetFrameworkDescription);
         }
+
+        public static bool IsMono()
+        {
+            const string MonoDescription = "Mono";
+            string frameworkDescription = RuntimeInformation.FrameworkDescription;
+            return frameworkDescription.StartsWith(MonoDescription);
+        }
     }
 }
